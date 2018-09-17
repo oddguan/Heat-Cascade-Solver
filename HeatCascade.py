f2c3a9660b5608de62d0919b364accd997d87f0d
Chenxiao Guan <1011zaozao@gmail.com>
2018-09-17 17:04:34 -0400

    finished heat cascade part. heat recovery work WIP

--- HeatCascade.py
+++ HeatCascade.py
@@ -33,7 +33,7 @@ class Stream():
         else:
             self.T_in_shifted = self.T_in + self.T_min/2
             self.T_out_shifted = self.T_out + self.T_min/2
-    
+
 
 class System():
     """
@@ -58,8 +58,8 @@ class System():
             if stream.T_in_shifted not in self.temperature_list:
                 self.temperature_list.append(stream.T_in_shifted)
             if stream.T_out_shifted not in self.temperature_list:
-                self.temperature_list.append(stream.T_out_shifted) 
-        
+                self.temperature_list.append(stream.T_out_shifted)
+
         return sorted(self.temperature_list, reverse=True)
 
 def formCpList(temperature_list, stream_list):
@@ -78,12 +78,14 @@ def formCpList(temperature_list, stream_list):
     for i in range(len(temperature_list)-1):
         for stream in stream_list:
             if stream.type == 'hot':
-                if stream.T_in_shifted>=temperature_list[i] and stream.T_out_shifted<=temperature_list[i+1]:
+                if stream.T_in_shifted>=temperature_list[i] and \
+                stream.T_out_shifted<=temperature_list[i+1]:
                     Cp_list[i]+=stream.Cp
             else:
-                if stream.T_in_shifted<=temperature_list[i+1] and stream.T_out_shifted>=temperature_list[i]:
+                if stream.T_in_shifted<=temperature_list[i+1] and \
+                stream.T_out_shifted>=temperature_list[i]:
                     Cp_list[i]-=stream.Cp
-    
+
     return Cp_list
 
 def calculateDeltaH(Cp_list, temperature_list):
@@ -92,9 +94,9 @@ def calculateDeltaH(Cp_list, temperature_list):
     delta H values for the heating cascade.
 
     Args:
-        Cp_list: A numpy array that stores the Cp for each temperature interval。
+        Cp_list: A numpy array that stores the Cp for each temperature interval.
         temperature_list: A list that stores the temperature intervals of the system.
-    
+
     Return:
         deltaH_list: A list that stores the enthalpy values for the heat cascade.
     """
@@ -102,7 +104,7 @@ def calculateDeltaH(Cp_list, temperature_list):
     for i in range(len(temperature_list)-1):
         delta_T = temperature_list[i] - temperature_list[i+1]
         deltaH_list.append(delta_T*Cp_list[i])
-    
+
     return deltaH_list
 
 def identifyPinch(deltaH_list):
@@ -113,24 +115,25 @@ def identifyPinch(deltaH_list):
         deltaH_list: a list that stores delta H values in order.
 
     Return:
-
+        low: what is the lowest infeasible power reached during the cascade process.
     """
-    H=0
-    for deltaH in deltaH_list:
-        H+=deltaH
-        if H<0:
-            print(H)
-            pinchValue = -H
-            
-
+    _sum = 0
+    low = 0
+    index = 0
+    for i in range(len(deltaH_list)):
+        _sum += deltaH_list[i]
+        if _sum<low:
+            low = _sum
+            index = i
+    return (low, index)
 
 
 def main():
     # Creating stream objects and system object
-    one = Stream(20, 135, 2)
-    two = Stream(170, 60, 3)
-    three = Stream(80, 140, 4)
-    four = Stream(150, 30, 1.5)
+    one = Stream(60, 180, 3)
+    two = Stream(180, 40, 2)
+    three = Stream(30, 105, 2.6)
+    four = Stream(150, 40, 4)
     streams = [one, two, three, four]
     for s in streams:
         s.shiftTemperature()
@@ -141,11 +144,19 @@ def main():
     print("Temperature intervals: " + str(temp_list))
     for i in range(len(temp_list)-1):
         delta_T = temp_list[i] - temp_list[i+1]
-    
+
     Cp_list = formCpList(temp_list, streams)
-    print("Cp values: " + str(Cp_list))
+    # print("Cp values: " + str(Cp_list))
     deltaH_list = calculateDeltaH(Cp_list, temp_list)
-    print("heat cascade values: " + str(deltaH_list))
+    print("Heat cascade values: " + str(deltaH_list))
+    coldUtility = sum(deltaH_list)
+    print("Heat dumps into the cold utility before adding pinch power: " + str(coldUtility) + "KW")
+    low, index = identifyPinch(deltaH_list)
+    print("The pinch occurs at interval #"+str(index+1)+", which is from ",\
+    str(temp_list[index+1]) + "C to ",str(temp_list[index]) + "C")
+    print("The pinch:", -low, "KW")
+    print("Heat dumps after adding pinch power:", coldUtility-low, "KW")
+
 
 if __name__ == '__main__':
     main()
